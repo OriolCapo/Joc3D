434972340b28a01f7ec2622bcd65edb066b58c64
OriolCapo <oriolcapo@MacBook-Pro-de-Oriol.local>
2017-05-24 23:47:39 +0200

    Afegeixo un panel amb información per cada cotxe
    Si mous amb les tecles esquerra i dreta, veuras els efectes

--- Rocket League/Assets/Scripts/SelectCars.cs
+++ Rocket League/Assets/Scripts/SelectCars.cs
@@ -1,29 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectCars : MonoBehaviour {
 
 	public float rotationSpeed = 10f;
 	public GameObject[] cars;//, car2, car3;
 
+	public Text carName;
+	public Text info;
+	public Image panel;
+
 	private Vector3 circumcentre;
 	private float radius;
 	private float angle;
-	private float timer;
 	private float subAngle;
 	private bool left, right;
 	private float idx;
 
+	private string[] carNames = new string[] { "Mustang", "Red Car", "SoftCar" };
+	private string[] infos = new string[] { "Orange", "Red", "Grey" };
 
 	void Start () {
 		circumcentre = new Vector3 (0.0f,2.0f,-23.09f);
 		radius = 23.09f;
 		angle = 0.0f;
-		timer = 0.0f;
 		left = false;
 		right = false;
 		idx = 0.0f;
+		setInformation ((int)idx);
 	}
 	
 
@@ -32,39 +38,44 @@ public class SelectCars : MonoBehaviour {
 		if (Input.GetKeyDown ("right") && !right && !left) {
 			right = true;
 			subAngle = 0.0f;
-			timer = idx * 2 * Mathf.PI / 3;
+			angle = idx * 2 * Mathf.PI / 3;
 			idx += 1;
 			if (idx == 3)
 				idx = 0;
+			hideInformation (false);
+			setInformation ((int)idx);
 		} else if (Input.GetKeyDown ("left") && !right && !left){
 			left = true;
 			subAngle = 0.0f;
-			timer = idx * 2 * Mathf.PI / 3;
+			angle = idx * 2 * Mathf.PI / 3;
 			idx -= 1;
 			if (idx == -1)
 				idx = 2;
+			hideInformation (false);
+			setInformation ((int)idx);
 		}
 		if (right) {
 			if (subAngle <= 2 * Mathf.PI / 3) {
 				subAngle += 2*Time.deltaTime;
-				timer += 2*Time.deltaTime;
+				angle += 2*Time.deltaTime;
 			} else {
 				right = false;
 				subAngle = 0.0f;
+				hideInformation (true);
 			}
 		}
 		else if (left){
 			if (subAngle <= 2 * Mathf.PI / 3) {
 				subAngle += 2*Time.deltaTime;
-				timer -= 2*Time.deltaTime;
+				angle -= 2*Time.deltaTime;
 
 			} else {
 				left = false;
 				subAngle = 0.0f;
+				hideInformation (true);
 			}
 		}
-
-		angle = timer;
+			
 		for (int i = 0; i <= 4; i += 2) {
 			rotateCar (cars [i / 2], angle + i * 2 * Mathf.PI / 3);
 		}
@@ -76,6 +87,17 @@ public class SelectCars : MonoBehaviour {
 		rotateCar (car2, angle + 2*Mathf.PI/3);
 		rotateCar (car3, angle + 2*2*Mathf.PI/3);
 		*/
+	}
+
+	void hideInformation(bool set){
+		panel.enabled = set;
+		carName.enabled = set;
+		info.enabled = set;
+	}
+
+	void setInformation(int idx){
+		carName.text = carNames [idx];
+		info.text = infos [idx];
 	}
 
 	void rotateCar(GameObject car, float angle) {
