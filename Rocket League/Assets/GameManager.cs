e76c1e2ca70aff57867b3deb3abee9a99c57be47
Dmnt <dmnt42@gmail.com>
2017-05-27 20:09:06 +0200

    Menú de pausa. Ajustaments a altres menus. En la selecció de cotxe, es pot confirmar apretant intro.

---
+++ Rocket League/Assets/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //----------------------------------------
+    // handles
+    public UIManager UI;
+
+    //-----------------------------------------
+    // function definitions    
+
+    // Use this for initialization
+    void Start()
+    {
+        UI.GetComponentInChildren<Canvas>().enabled = false;
+    }
+
+    public void TogglePauseMenu()
+    {
+        // not the optimal way but for the sake of readability
+        if (UI.GetComponentInChildren<Canvas>().enabled)
+        {
+            UI.GetComponentInChildren<Canvas>().enabled = false;
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            UI.GetComponentInChildren<Canvas>().enabled = true;
+            Time.timeScale = 0f;
+        }
+
+        Debug.Log("GAMEMANAGER:: TimeScale: " + Time.timeScale);
+    }
+
+}
---
+++ Rocket League/Assets/ManagePause.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagePause : MonoBehaviour {
+
+    public GameObject pausepanel;
+    public GameObject pauseshade;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("space"))
+        {
+            pauseshade.SetActive(true);
+            pausepanel.SetActive(true);
+            //Time.timeScale = 0;
+
+        }
+
+
+
+    }
+}
